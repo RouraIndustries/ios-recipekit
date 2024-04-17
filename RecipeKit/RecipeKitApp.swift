560733afdfdea1059f943be2d12f091548e33c3c
Christopher J. Roura <cjroura@gmail.com>
2024-04-17 19:23:20 -0400

    Initial Commit

---
+++ RecipeKit/RecipeKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeKitApp.swift
+//  RecipeKit
+//
+//  Created by Christopher J. Roura on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
