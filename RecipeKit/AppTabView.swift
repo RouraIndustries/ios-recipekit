ff08aec12febdd0c35d018e80282b4357936f9bd
Christopher J. Roura <cjroura@gmail.com>
2024-04-17 20:02:50 -0400

    [IRK-15] Create TabBarView

---
+++ RecipeKit/AppTabView.swift
@@ -0,0 +1,20 @@
+//
+//  AppTabView.swift
+//  RecipeKit
+//
+//  Created by Christopher J. Roura on 4/17/24.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    AppTabView()
+}
