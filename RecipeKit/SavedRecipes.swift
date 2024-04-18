a933cf1b179fac48ae3b1f7c17f9611b34622969
Christopher J. Roura <cjroura@gmail.com>
2024-04-18 17:46:08 -0400

    [IRK-45] Define Saved Recipes Data Model

---
+++ RecipeKit/SavedRecipes.swift
@@ -0,0 +1,18 @@
+//
+//  SavedRecipes.swift
+//  RecipeKit
+//
+//  Created by Christopher J. Roura on 4/18/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class SavedRecipes {
+    var savedRecipes: [Recipe]
+
+    init(savedRecipes: [Recipe]) {
+        self.savedRecipes = savedRecipes
+    }
+}
