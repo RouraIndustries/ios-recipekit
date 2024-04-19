0592d50e8c304297ccec9be0f0c74e7f8791dc76
Christopher J. Roura <cjroura@gmail.com>
2024-04-19 13:34:50 -0400

    [IRK-54] Define Macro Data Model

---
+++ RecipeKit/Macro.swift
@@ -0,0 +1,22 @@
+//
+//  Macro.swift
+//  RecipeKit
+//
+//  Created by Christopher J. Roura on 4/19/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Macro {
+    var name: String
+    var quantity: Double
+
+    var recipe: Recipe?
+
+    init(name: String, quantity: Double) {
+        self.name = name
+        self.quantity = quantity
+    }
+}
