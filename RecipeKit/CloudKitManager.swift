55b993f6ebef1b25c972de897fe9278784b726b5
Christopher J. Roura <cjroura@gmail.com>
2024-04-25 20:51:50 -0400

    [IRK-71] Add UserOperations and integrate into CloudKitManager

--- RecipeKit/CloudKitManager.swift
+++ RecipeKit/CloudKitManager.swift
@@ -10,4 +10,31 @@ import Observation
 import SwiftUI
 
 @Observable final class CloudKitManager {
+    var userRecord: CKRecord?
+    var profileRecordID: CKRecord.ID?
+
+    private let container = CKContainer.default()
+    private let resultsLimit = 25
+
+    init() {
+        Task { await fetchUserRecord() }
+    }
+
+    func fetchUserRecord() async {
+        try? await getUserRecord()
+    }
+}
+
+// MARK: - CloudKitManager-UserOperations
+
+extension CloudKitManager: UserOperations {
+    @MainActor
+    func getUserRecord() async throws {
+        let recordID = try await container.userRecordID()
+        let record = try await container.publicCloudDatabase.record(for: recordID)
+        userRecord = record
+
+        guard let profileReference = record["userProfile"] as? CKRecord.Reference else { return }
+        profileRecordID = profileReference.recordID
+    }
 }
