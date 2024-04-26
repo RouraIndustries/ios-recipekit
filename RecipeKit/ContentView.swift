692af89fb8961fb6ed20d4af5b6d7b798a058d65
Christopher J. Roura <cjroura@gmail.com>
2024-04-25 20:21:51 -0400

    Add CloudKitManager class as an EnvironmentObject, injected in ContentView

--- RecipeKit/ContentView.swift
+++ RecipeKit/ContentView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let cloudKitManager = CloudKitManager()
+
     var body: some View {
         AppTabView()
+            .environmentObject(cloudKitManager)
     }
 }
 
