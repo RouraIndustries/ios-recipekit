696ee47ee142cfd2b5fc723a1def5e5019aa0026
Christopher J. Roura <cjroura@gmail.com>
2024-04-25 20:35:24 -0400

    [IRK-69] Prototype ProfileView

--- RecipeKit/ProfileView.swift
+++ RecipeKit/ProfileView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ProfileView: View {
+//    let profile: V0_Profile
+
     var body: some View {
-        Text("Profile")
+        ScrollView {
+            VStack {
+                HStack {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 72.0, height: 72.0)
+                        .clipShape(.circle)
+
+                    VStack(alignment: .leading) {
+                        Text("First name")
+                        Text("Last name")
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16.0)
+                .background(.gray)
+                .clipShape(.rect(cornerRadius: 12.0))
+            }
+            .padding(.horizontal, 16.0)
+            .padding(.vertical, 12.0)
+        }
     }
 }
 
 #Preview {
     ProfileView()
+//    ProfileView(profile: V0_Profile(record: ))
 }
