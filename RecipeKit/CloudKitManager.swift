fbdf4a7ad9420d7e186832f939b2abe4ada2d42e
Christopher J. Roura <cjroura@gmail.com>
2024-04-25 20:21:51 -0400

    [IRK-68] Add CloudKitManager class as an EnvironmentObject, injected in ContentView

---
+++ RecipeKit/CloudKitManager.swift
@@ -0,0 +1,13 @@
+//
+//  CloudKitManager.swift
+//  RecipeKit
+//
+//  Created by Christopher J. Roura on 4/25/24.
+//
+
+import CloudKit
+import Observation
+import SwiftUI
+
+@Observable final class CloudKitManager {
+}
