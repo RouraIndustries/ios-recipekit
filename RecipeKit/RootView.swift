7cc2149f2eb4b544a62df37ab03857a9af06cf3e
Christopher J. Roura <cjroura@gmail.com>
2024-04-27 15:12:13 -0400

    [IRK-74] Create RootView with logic for showing OnboardView or TabView

---
+++ RecipeKit/RootView.swift
@@ -0,0 +1,37 @@
+//
+//  RootView.swift
+//  RecipeKit
+//
+//  Created by Christopher J. Roura on 4/27/24.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    static private let kHasSeenOnboardView = "hasSeenOnboardView"
+    @Environment(\.cloudKitManager) private var cloudKitManager
+
+    @State private var isShowingOnboardView = false
+    @AppStorage(kHasSeenOnboardView) var hasSeenOnboardView = false {
+        didSet { isShowingOnboardView = hasSeenOnboardView }
+    }
+
+    var body: some View {
+        AppTabView()
+            .onAppear { runStartupChecks() }
+            .sheet(isPresented: $isShowingOnboardView) {
+//                OnboardView()
+            }
+    }
+
+    func runStartupChecks() {
+        if !hasSeenOnboardView {
+            isShowingOnboardView = true
+            hasSeenOnboardView = true
+        }
+    }
+}
+
+#Preview {
+    RootView()
+}
