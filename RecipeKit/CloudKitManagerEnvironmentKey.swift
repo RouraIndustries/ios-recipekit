b22488b0deb3c37b962071f19c37e81dbcd91639
Christopher J. Roura <cjroura@gmail.com>
2024-04-28 00:52:33 -0400

    [IRK-70] Fix CloudKitManager environmentObject injection, Convert to EnvironmentKey

---
+++ RecipeKit/CloudKitManagerEnvironmentKey.swift
@@ -0,0 +1,18 @@
+//
+//  CloudKitManagerEnvironmentKey.swift
+//  RecipeKit
+//
+//  Created by Christopher J. Roura on 4/28/24.
+//
+
+import SwiftUI
+
+struct CloudKitManagerEnvironmentKey: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudKitManagerEnvironmentKey()
+}
