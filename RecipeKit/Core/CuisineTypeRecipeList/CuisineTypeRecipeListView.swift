cde0c2a05911b44a1e7ca39d2265a5f4ac0b0a02
Christopher J. Roura <cjroura@gmail.com>
2024-05-06 19:52:08 -0400

    [IRK-96] Add tuxedo styling to CuisineTypeRecipeListView

--- RecipeKit/Core/CuisineTypeRecipeList/CuisineTypeRecipeListView.swift
+++ RecipeKit/Core/CuisineTypeRecipeList/CuisineTypeRecipeListView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CloudKit
+import Tuxedo
 
 struct CuisineTypeRecipeListView: View {
     @Environment(\.dismiss) private var dismiss
@@ -27,8 +28,31 @@ struct CuisineTypeRecipeListView: View {
             }
             .scrollTargetLayout()
         }
-        .contentMargins(.horizontal, 16.0, for: .scrollContent)
         .scrollTargetBehavior(.viewAligned)
+        .contentMargins(.horizontal, 16.0, for: .scrollContent)
+        .padding(.vertical, 12.0)
+        .background(.backgroundPrimary)
+        .navigationBarBackButtonHidden()
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                HStack(spacing: .zero) {
+                    Button { dismiss() } label: {
+                        Image(systemName: "chevron.left")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 14, height: 14)
+                            .foregroundStyle(.midnightPrimary)
+                            .fontWeight(.bold)
+                    }
+                    .frame(width: 34, height: 50, alignment: .leading)
+
+                    Text(cuisineType.cuisineType.rawValue)
+                        .tuxedoFont(.h5Bold)
+                        .foregroundStyle(.foregroundPrimary)
+                }
+            }
+        }
     }
 }
 
