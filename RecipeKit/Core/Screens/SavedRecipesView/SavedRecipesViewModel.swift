4dd5ee98c3444c3224e2a85032f5ae882c1adc64
Christopher J. Roura <cjroura@gmail.com>
2024-05-06 20:11:46 -0400

    [IRK-105] Add SavedRecipesViewModel to handle saving and removing recipes

--- RecipeKit/Core/Screens/SavedRecipesView/SavedRecipesViewModel.swift
+++ RecipeKit/Core/Screens/SavedRecipesView/SavedRecipesViewModel.swift
@@ -9,6 +9,42 @@ import Foundation
 import SwiftData
 
 extension SavedRecipesView {
-    @Observable final class ViewModel: ObservableObject {
+    @Observable final class ViewModel {
+        var recipeManager: RecipeManager?
+        var cloudKitManager: CloudKitManager?
+        
+        var isLoading = false
+        var showError = false
+        var error: AlertItem? {
+            didSet { showError.toggle() }
+        }
+        
+        func fetchDataTaskGroup() async {
+            guard let recipeManager, let cloudKitManager else { return }
+            showLoadingView()
+
+            async let cuisineTypes = cloudKitManager.getCuisineTypes()
+            async let recipes = cloudKitManager.getRecipes()
+            async let mealTypes = cloudKitManager.getMealTypes()
+            async let macros = cloudKitManager.getMacros()
+
+            do {
+                let (cuisineTypesResult, recipesResult, mealTypesResult, macrosResult) =
+                try await (cuisineTypes, recipes, mealTypes, macros)
+
+                recipeManager.cuisineTypes = cuisineTypesResult
+                recipeManager.recipes = recipesResult
+                recipeManager.mealTypes = mealTypesResult
+                recipeManager.macros = macrosResult
+
+                hideLoadingView()
+            } catch {
+                hideLoadingView()
+                                self.error = AlertContext.unableToFetchData
+            }
+        }
+
+        private func showLoadingView() { Task { @MainActor in isLoading = true }}
+        private func hideLoadingView() { Task { @MainActor in isLoading = false }}
     }
 }
