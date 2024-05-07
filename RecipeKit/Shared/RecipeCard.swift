6c7dc642bf97cdceaf36e1e910caaefb79253afd
Christopher J. Roura <cjroura@gmail.com>
2024-05-06 20:22:56 -0400

    [IRK-110] Add tuxedo styling to RecipeCard

--- RecipeKit/Shared/RecipeCard.swift
+++ RecipeKit/Shared/RecipeCard.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CloudKit
+import Tuxedo
 
 enum CardStyle {
     case small
@@ -15,8 +16,21 @@ enum CardStyle {
 }
 
 struct RecipeCard: View {
+    @Environment(\.recipeManager) private var recipeManager
+
     var style: CardStyle = .standard
     let recipe: V0_Recipe
+
+    var mealType: String {
+        recipeManager.mealTypes
+            .first { $0.ckRecordID == recipe.mealType?.recordID }?
+            .recipeMealType.rawValue ?? ""
+    }
+
+    var calories: String {
+        let caloriesForRecipe = recipeManager.macros.filter { $0.recipe?.recordID == recipe.ckRecordID }.first
+        return "\(caloriesForRecipe?.macroName ?? "") \(caloriesForRecipe?.macroUnitLabel ?? "")"
+    }
 
     var body: some View {
         switch style {
@@ -27,33 +41,50 @@ struct RecipeCard: View {
     }
 
     var small: some View {
-        ZStack(alignment: .bottomLeading) {
+        HStack(spacing: 16.0) {
             recipe.squareImage
                 .resizable()
                 .scaledToFill()
-                .frame(
-                    minWidth: .zero,
-                    maxWidth: .infinity,
-                    minHeight: .zero,
-                    maxHeight: .infinity
-                )
+                .frame(width: 96.0, height: 96.0)
+                .clipShape(.rect(cornerRadius: 16.0))
 
-            VStack(alignment: .leading, spacing: 8.0) {
-                Text("Recipe")
-                    .fontWeight(.bold)
+            VStack(alignment: .leading, spacing: 4.0) {
+                Text(recipe.recipeTitle)
+                    .tuxedoFont(.h5Bold)
+                    .foregroundStyle(.foregroundPrimary)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.75)
+                    .multilineTextAlignment(.leading)
 
-                Text("Recipe")
-                    .font(.subheadline)
+                Text(mealType)
+                    .tuxedoFont(.bodyExtraBold)
+                    .foregroundStyle(.foregroundSecondary)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+
+                Group {
+                    Text(calories)
+                        .tuxedoFont(.captionExtraBold)
+                        .foregroundStyle(.foregroundSecondary)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                }
             }
-            .frame(height: 78)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 16)
-            .background(.background)
+
+            Spacer()
+
+            Image(systemName: "chevron.right")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 14.0, height: 14.0)
+                .foregroundStyle(.foregroundDisabled)
+                .fontWeight(.bold)
         }
-        .frame(width: 220, height: 204)
+        .padding(.horizontal, 16.0)
+        .padding(.vertical, 12.0)
+        .background(.backgroundRecessed)
         .clipShape(.rect(cornerRadius: 16.0))
-        .padding(.bottom, 16.0)
-        .shadow(color: Color(red: 23 / 255, green: 27 / 255, blue: 31 / 255, opacity: 0.16), radius: 8, y: 4)
+        .shadow(color: .shadow, radius: 8, y: 4)
     }
 
     var standard: some View {
@@ -68,22 +99,37 @@ struct RecipeCard: View {
                     maxHeight: .infinity
                 )
 
-            VStack(alignment: .leading, spacing: 8.0) {
-                Text("Recipe")
-                    .fontWeight(.bold)
+            VStack(alignment: .leading, spacing: 4.0) {
+                Text(recipe.recipeTitle)
+                    .tuxedoFont(.h5Bold)
+                    .foregroundStyle(.foregroundPrimary)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
 
-                Text("Recipe")
-                    .font(.subheadline)
+                Text(mealType)
+                    .tuxedoFont(.bodyExtraBold)
+                    .foregroundStyle(.foregroundSecondary)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+
+                Group {
+                    Text(calories)
+                        .tuxedoFont(.captionExtraBold)
+                        .foregroundStyle(.foregroundSecondary)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                }
             }
-            .frame(height: 78)
+            .frame(height: 80)
             .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 16)
-            .background(.background)
+            .padding(.horizontal, 12.0)
+            .background(.ultraThinMaterial)
+            .background(.backgroundPrimary.opacity(0.8))
         }
         .frame(width: 220, height: 204)
         .clipShape(.rect(cornerRadius: 16.0))
         .padding(.bottom, 16.0)
-        .shadow(color: Color(red: 23 / 255, green: 27 / 255, blue: 31 / 255, opacity: 0.16), radius: 8, y: 4)
+        .shadow(color: .shadow, radius: 8, y: 4)
     }
 
     var large: some View {
@@ -99,25 +145,41 @@ struct RecipeCard: View {
                 )
 
             VStack(alignment: .leading, spacing: 8.0) {
-                Text("Recipe")
-                    .fontWeight(.bold)
+                Text(recipe.recipeTitle)
+                    .tuxedoFont(.h5Bold)
+                    .foregroundStyle(.foregroundPrimary)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.75)
+                    .multilineTextAlignment(.leading)
 
-                Text("Recipe")
-                    .font(.subheadline)
+                Text(mealType)
+                    .tuxedoFont(.h5Light)
+                    .foregroundStyle(.foregroundSecondary)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+
+                Group {
+                    Text(calories)
+                        .tuxedoFont(.bodyBold)
+                        .foregroundStyle(.foregroundSecondary)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                }
             }
-            .frame(height: 78)
+            .frame(height: 96)
             .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 16)
-            .background(.background)
+            .padding(.horizontal, 12)
+            .background(.ultraThinMaterial)
+            .background(.backgroundPrimary.opacity(0.8))
         }
         .frame(maxWidth: .infinity)
         .frame(height: 320)
         .clipShape(.rect(cornerRadius: 16.0))
         .padding(.bottom, 16.0)
-        .shadow(color: Color(red: 23 / 255, green: 27 / 255, blue: 31 / 255, opacity: 0.16), radius: 8, y: 4)
+        .shadow(color: .shadow, radius: 8, y: 4)
     }
 }
 
 #Preview {
-    RecipeCard(recipe: V0_Recipe(record: CKRecord(recordType: RecordType.recipe)))
+    RecipeCard(style: .small, recipe: V0_Recipe(record: CKRecord(recordType: RecordType.recipe)))
 }
