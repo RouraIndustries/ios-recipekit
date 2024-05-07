c8832ed6734f3e7ff2d8ca6d1f59aa8017609b4c
Christopher J. Roura <cjroura@gmail.com>
2024-05-06 20:24:35 -0400

    [IRK-111] Add tuxedo style to LoadingView

--- RecipeKit/Shared/LoadingView.swift
+++ RecipeKit/Shared/LoadingView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import Tuxedo
 
 struct LoadingView: View {
     var body: some View {
-        ProgressView()
+        VStack(spacing: 12.0) {
+            ProgressView()
+                .foregroundStyle(.midnightPrimary)
+
+            Text("Loading...")
+                .tuxedoFont(.bodyExtraBold)
+                .foregroundStyle(.foregroundPrimary)
+        }
+        .padding(.horizontal, 32.0)
+        .padding(.vertical, 32.0)
+        .background(.backgroundPrimary)
+        .clipShape(.rect(cornerRadius: 12.0))
+        .overlay {
+            RoundedRectangle(cornerRadius: 12.0)
+                .stroke(.foregroundDisabled, lineWidth: 1.0)
+        }
+        .shadow(color: .shadow, radius: 8, y: 4)
+        .transition(.move(edge: .bottom).combined(with: .opacity))
     }
 }
 
